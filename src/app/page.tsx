b93100ee9f1384c2fb2f4c99281494f2ab40b6ee
Tamas Nemes <nemestomi2@gmail.com>
2024-03-17 23:11:56 +0100

    Fix issue with global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Image
-      className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+      className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
       src="/next.svg"
       alt="Next.js Logo"
       width={180}
