8451bd00cf63acc7b09bc23b86a8008b484f0fe4
Tamas Nemes <nemestomi2@gmail.com>
2024-03-17 13:42:31 +0100

    Extract fonts in separate file

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Julius_Sans_One, Saira } from "next/font/google";
+
+export const primary_font = Saira({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
+
+export const secondary_font = Julius_Sans_One({
+    display: "block",
+    weight: "400",
+    subsets: ["latin"],
+});
