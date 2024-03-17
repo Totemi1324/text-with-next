e3cf5ce5549b580aa7921bfb89b5c57911dff3f5
Tamas Nemes <nemestomi2@gmail.com>
2024-03-17 20:51:48 +0100

    Update theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
@@ -15,6 +16,83 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      themes: {
+        "dark-custom": {
+          extend: "dark", // Inherit default values from the dark theme
+          colors: {
+            background: "#070707",
+            foreground: "#ffffff",
+            primary: {
+              DEFAULT: "#f5a524",
+              50: "#302311",
+              100: "#5c4018",
+              200: "#8c601e",
+              300: "#bf8222",
+              400: "#f5a524",
+              500: "#f9af43",
+              600: "#fcb95c",
+              700: "#fec274",
+              800: "#ffcc8b",
+              900: "#ffd6a2",
+              foreground: "#ffffff", // Assuming you meant the text color for primary elements
+            },
+            secondary: {
+              DEFAULT: "#2474f5",
+              50: "#161b2f",
+              100: "#1f2f5b",
+              200: "#25458b",
+              300: "#275cbf",
+              400: "#2474f5",
+              500: "#5282f7",
+              600: "#6f91f8",
+              700: "#889ffa",
+              800: "#9eaffb",
+              900: "#b3befc",
+              foreground: "#ffffff", // Assuming you meant the text color for secondary elements
+            },
+            focus: "#f9af43",
+          },
+        },
+        "light-custom": {
+          extend: "light", // Inherit default values from the light theme
+          colors: {
+            background: "#ffffff",
+            foreground: "#070707",
+            primary: {
+              DEFAULT: "#f5a524",
+              50: "#302311",
+              100: "#5c4018",
+              200: "#8c601e",
+              300: "#bf8222",
+              400: "#f5a524",
+              500: "#f9af43",
+              600: "#fcb95c",
+              700: "#fec274",
+              800: "#ffcc8b",
+              900: "#ffd6a2",
+              foreground: "#ffffff", // Assuming you meant the text color for primary elements
+            },
+            secondary: {
+              DEFAULT: "#2474f5",
+              50: "#161b2f",
+              100: "#1f2f5b",
+              200: "#25458b",
+              300: "#275cbf",
+              400: "#2474f5",
+              500: "#5282f7",
+              600: "#6f91f8",
+              700: "#889ffa",
+              800: "#9eaffb",
+              900: "#b3befc",
+              foreground: "#ffffff", // Assuming you meant the text color for secondary elements
+            },
+            focus: "#f9af43",
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
