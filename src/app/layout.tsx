cdf7cfbff5621f91b70ec9d408b26b7eaadbbff1
Tamas Nemes <nemestomi2@gmail.com>
2024-03-20 10:32:13 +0100

    Remove unnecessary inputs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,12 @@ import { Providers } from "@/app/providers";
 import { primary_font } from "@/app/ui/fonts";
 import Footer from "@/components/Footer";
 
-import clsx from 'clsx';
-import { useAtom } from "jotai";
-
-import { themeAtom } from '@/app/stores/atoms';
 import "./globals.css";
 
-/*export const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Text with Next",
   description: "The story you continue is in your hands. Whatever you'll do, you can text - with Next!",
-};*/
-/*const [theme] = useAtom(themeAtom);
-
-  const themeClasses = clsx({
-    'dark-orange-blue': theme === 'dark',
-    'dark': theme === 'dark',
-    'light-orange-blue': theme === 'light',
-    'light': theme === 'light',
-  });*/
+};
 
 export default function RootLayout({
   children,
