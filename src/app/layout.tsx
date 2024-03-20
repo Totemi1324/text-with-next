3ef4200422a2229ad3406094fd29ace15d5b9b9f
Tamas Nemes <nemestomi2@gmail.com>
2024-03-20 10:03:58 +0100

    Make theming toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,37 @@
+"use client";
+
 import type { Metadata } from "next";
-import "./globals.css";
+
+import { Providers } from "@/app/providers";
 import { primary_font } from "@/app/ui/fonts";
-import { Providers } from "./providers";
 import Footer from "@/components/Footer";
 
-export const metadata: Metadata = {
+import clsx from 'clsx';
+import { useAtom } from "jotai";
+
+import { themeAtom } from '@/app/stores/atoms';
+import "./globals.css";
+
+/*export const metadata: Metadata = {
   title: "Text with Next",
   description: "The story you continue is in your hands. Whatever you'll do, you can text - with Next!",
-};
+};*/
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [theme] = useAtom(themeAtom);
+
+  const themeClasses = clsx({
+    'dark-orange-blue': theme === 'dark',
+    'dark': theme === 'dark',
+    'light-orange-blue': theme === 'light',
+    'light': theme === 'light',
+  });
   return (
-    <html lang="en" className="dark-orange-blue dark">
+    <html lang="en" className={themeClasses}>
       <body className={primary_font.className}>
         <Providers>
           <div className="flex flex-col min-h-screen">
